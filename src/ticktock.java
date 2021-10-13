e44044be9b87683b94a2aa0e655e8719cb773bba
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-10-13 09:29:09 +0200

    amen

--- src/ticktock.java
+++ src/ticktock.java
@@ -5,31 +5,50 @@ public class ticktock {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("****************************");
+        System.out.println("");
         System.out.println("Härzlich Wihlkommah beih TickTockZehen");
+        System.out.println("");
         System.out.println("****************************");
-        int[][] spfeld = new int[3][3];
-        String sep = "/";
-        for (int i = 0; i < spfeld.length; i++) {
-            System.out.println(spfeld[i][0]+ sep);
-            for (int b = 0; b < spfeld.length; b++) {
-                System.out.println(spfeld[0][b]+ sep);
+        boolean gwunna = false;
+        char[][] spfeld = new char[3][3];
+        spfeld[0][0] = 1;
+        spfeld[1][1] = 1;
+        int cplay = 1;
+
+        while (!gwunna){
+            System.out.println("Entehr your choice Player " + cplay);
+            String sep = "|";
+            for (int i = 0; i < spfeld.length; i++) {
+                int r1 = spfeld[i][0];
+                int r2 = spfeld[i][1];
+                int r3 = spfeld[i][2];
+
+                System.out.println(sep + r1 + sep + r2 + sep + r3 + sep);
+            }
+            String auswahl = scanner.nextLine();
+            String[] auswal = auswahl.split(",");
+
+            if (cplay == 1){
+                spfeld[Integer.parseInt(auswal[0])][Integer.parseInt(auswal[1])] = 1;
+                cplay = 2;
+            }
+            else if (cplay == 2){
+                spfeld[Integer.parseInt(auswal[0])][Integer.parseInt(auswal[1])] = 2;
+                cplay = 1;
+            }
+            if((spfeld[0][0] + spfeld[0][1] + spfeld[0][2]) == 3){
+                gwunna = true;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if(gwunna == true){
+            if(cplay == 1){
+                cplay = 2;
+            }
+            else{
+                cplay = 1;
+            }
+        }
+        System.out.println("Gratuliere Spieler Nr " + cplay);
 
         /* System.out.println("Rädy Player One?");
         String input = scanner.nextLine();
@@ -44,4 +63,5 @@ public class ticktock {
         } */
 
     }
+
 }

d1667583196afa78ceab852d2fec6414006dfffe
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-10-20 09:29:03 +0200

    amen

--- src/com/smortify/michael/projekte/totermann.java
+++ src/com/smortify/michael/projekte/totermann.java
@@ -4,6 +4,32 @@ import java.util.Scanner;
 
 public class totermann {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        char[] Wort = {'t','e','s','t'};
+        char[] zen = {'*','*','*','*'};
+
+        String fullW = "";
+        boolean win = false;
+
+        while (!win){
+            char nt = scanner.next().charAt(0);
+
+            for (int i = 0; i < Wort.length; i++) {
+                if (Wort[i] == nt){
+                    zen[i] = Wort[i];
+                    System.out.println("Buchstabe gefunden");
+
+                }
+
+            }
+
+            for (int bi = 0; bi < zen.length; bi++) {
+                fullW = String.valueOf(zen[bi]);
+
+            }
+            System.out.println(fullW);
+
+        }
     }
 }

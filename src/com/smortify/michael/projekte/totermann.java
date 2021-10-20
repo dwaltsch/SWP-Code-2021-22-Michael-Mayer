5fdc58e9593f6500931de040cec4b4c7c38714dc
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-10-20 08:50:00 +0200

    amen

--- src/com/smortify/michael/projekte/totermann.java
+++ src/com/smortify/michael/projekte/totermann.java
@@ -4,42 +4,6 @@ import java.util.Scanner;
 
 public class totermann {
     public static void main(String[] args) {
-        String dings = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Wenn du das Wort ned ussafindest stribt ein Mann");
-        System.out.println("Hier dein Wort");
-        String[] worter = null;
-        String[] an = null;
-        an = new String[3];
-        worter = new String[3];
-        worter[0] = "test";
-        worter[1] = "testaberlang";
-        for (int i = 0; i < worter[0].length(); i++) {
-            dings = dings + "*";
-        }
-        an[0] = dings;
-        Random random = new Random();
-        int auswahl = random.nextInt(1);
-
-        System.out.println(worter[auswahl]);
-        System.out.println(an[0]);
-
-        String auswal = scanner.nextLine();
-
-        for (int bi = 0; bi < an.length; bi++) {
-            if (auswal == worter[bi]){
-                System.out.println("War drinnen");
-                System.out.println(bi);
-                String ja = an[0];
-                char[] jars = ja.toCharArray();
-                jars[bi] = worter[0].charAt(0);
-                an[0] = jars.toString();
-                System.out.println(an[0]);
-            }
-            else {
-                System.out.println("no");
-            }
-        }
 
     }
 }

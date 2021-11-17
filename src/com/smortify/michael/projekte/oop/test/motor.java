931301daa507eaaa5ea9276f752d4e8627e17b11
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-11-17 08:16:59 +0100

    amen

--- src/com/smortify/michael/projekte/oop/test/motor.java
+++ src/com/smortify/michael/projekte/oop/test/motor.java
@@ -3,7 +3,12 @@ package com.smortify.michael.projekte.oop.test;
 public class motor {
     private enum TYPE{DIESEL,BENZIN}
     private int ps;
-    private String type;
+    private TYPE type;
+
+    public motor(int ps, TYPE type) {
+        this.ps = ps;
+        this.type = type;
+    }
 
     public void fahren(int anzahl){
         System.out.println("Da motor läuft mit");

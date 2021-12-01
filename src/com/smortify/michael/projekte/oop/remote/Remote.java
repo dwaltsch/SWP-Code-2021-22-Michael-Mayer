1ea297b67bba0d165fe571a50ea464b576051acf
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-12-01 09:29:28 +0100

    remote fast finished

--- src/com/smortify/michael/projekte/oop/remote/Remote.java
+++ src/com/smortify/michael/projekte/oop/remote/Remote.java
@@ -1,4 +1,33 @@
 package com.smortify.michael.projekte.oop.remote;
 
+import com.smortify.michael.projekte.oop.remote.battery.Battery;
+
 public class Remote {
+    private boolean isOn;
+    private boolean hasPower;
+    private Battery battery1;
+    private Battery battery2;
+
+    public Remote(boolean isOn, boolean hasPower, Battery battery1, Battery battery2) {
+        this.isOn = isOn;
+        this.hasPower = hasPower;
+        this.battery1 = battery1;
+        this.battery2 = battery2;
+    }
+
+    public void turnOn(){
+        battery1.setChargingStatus(battery1.getChargingStatus()-5);
+        battery2.setChargingStatus(battery2.getChargingStatus()-5);
+        battery1.turnonstatus();
+        battery2.turnonstatus();
+    }
+    public void turnOff(){
+        battery1.turnoffstatus();
+        battery2.turnoffstatus();
+    }
+    public int getStatus(){
+        int status = battery1.getChargingStatus() + battery2.getChargingStatus();
+        return status;
+    }
+
 }
--- src/com/smortify/michael/projekte/oop/remote/battery/Battery.java
+++ src/com/smortify/michael/projekte/oop/remote/battery/Battery.java
@@ -1,5 +1,26 @@
 package com.smortify.michael.projekte.oop.remote.battery;
 
 public class Battery {
+    private int chargingStatus;
+
+    public void turnonstatus(){
+        System.out.println("Verbraucher angeschlossen");
+    }
+    public Battery(int chargingStatus) {
+        this.chargingStatus = chargingStatus;
+    }
+
+    public int getChargingStatus() {
+        return chargingStatus;
+    }
+
+    public void setChargingStatus(int chargingStatus) {
+        this.chargingStatus = chargingStatus;
+    }
+
+    public void turnoffstatus(){
+        System.out.println("Kein Verbraucher angeschlossen");
+        System.out.println(this.chargingStatus);
+    }
 
 }

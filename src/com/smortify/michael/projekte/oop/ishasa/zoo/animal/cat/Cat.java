0d655f3b8dd2ddf3cecd495192665c1b6a8c6066
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-12-10 12:17:40 +0100

    alles instanziert und verschönert

---
+++ src/com/smortify/michael/projekte/oop/ishasa/zoo/animal/cat/Cat.java
@@ -0,0 +1,13 @@
+package com.smortify.michael.projekte.oop.ishasa.zoo.animal.cat;
+
+import com.smortify.michael.projekte.oop.ishasa.zoo.animal.Animal;
+
+public class Cat extends Animal {
+    public Cat(int weight, int height) {
+        super(weight, height);
+    }
+
+    public void miau() {
+
+    }
+}
---
+++ src/com/smortify/michael/projekte/oop/ishasa/zoo/animal/dog/Dog.java
@@ -0,0 +1,13 @@
+package com.smortify.michael.projekte.oop.ishasa.zoo.animal.dog;
+
+import com.smortify.michael.projekte.oop.ishasa.zoo.animal.Animal;
+
+public class Dog extends Animal {
+    public Dog(int weight, int height) {
+        super(weight, height);
+    }
+
+    public void bark() {
+
+    }
+}

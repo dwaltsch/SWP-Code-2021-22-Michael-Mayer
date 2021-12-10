a4e14169593994683f57af70ca028256d10d63b4
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-12-10 12:02:53 +0100

    alles instanziert und so

--- src/com/smortify/michael/projekte/oop/ishasa/Main.java
+++ src/com/smortify/michael/projekte/oop/ishasa/Main.java
@@ -1,4 +1,16 @@
 package com.smortify.michael.projekte.oop.ishasa;
 
+import com.smortify.michael.projekte.oop.ishasa.zoo.Zoo;
+import com.smortify.michael.projekte.oop.ishasa.zoo.animal.Animal;
+
 public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Animal(40,20);
+        Animal a2 = new Animal(50,30);
+
+        Zoo zoo = new Zoo("Kinderzoo", "Grosestrase");
+        zoo.addAnimal(a1);
+        zoo.addAnimal(a2);
+        zoo.printAnimal();
+    }
 }
--- src/com/smortify/michael/projekte/oop/ishasa/zoo/Zoo.java
+++ src/com/smortify/michael/projekte/oop/ishasa/zoo/Zoo.java
@@ -1,15 +1,23 @@
 package com.smortify.michael.projekte.oop.ishasa.zoo;
+import com.smortify.michael.projekte.oop.ishasa.zoo.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Zoo {
     private String name;
     private String street;
+    private List<Animal> animals;
+
 
     public void makeShow(){
         System.out.println("The show has just started");
     }
+
     public Zoo(String name, String street) {
         this.name = name;
         this.street = street;
+        this.animals = new ArrayList<>();
     }
 
     public String getName() {
@@ -26,5 +34,13 @@ public class Zoo {
 
     public void setStreet(String street) {
         this.street = street;
+    }
+    public void addAnimal(Animal animal){
+        this.animals.add(animal);
+    }
+    public void printAnimal(){
+        for(Animal animal: this.animals){
+            System.out.println("Vieh " + animal.getHeight() + " " + animal.getWeight() + " ");
+        }
     }
 }

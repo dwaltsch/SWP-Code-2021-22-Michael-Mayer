16359e613ce91cb8cb0a00bc4897861830d4ab8c
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-01-19 08:55:33 +0100

    Handy example

---
+++ src/com/smortify/michael/projekte/oop/testpractice/Camera.java
@@ -0,0 +1,12 @@
+package com.smortify.michael.projekte.oop.testpractice;
+
+public class Camera {
+    private int id;
+    public void takePicture(){
+        System.out.println("WAAAAAAAAAAAAAAAAAAAAAAAAAASSSS");
+    }
+
+    public Camera(int id) {
+        this.id = id;
+    }
+}
---
+++ src/com/smortify/michael/projekte/oop/testpractice/Main.java
@@ -0,0 +1,10 @@
+package com.smortify.michael.projekte.oop.testpractice;
+
+public class Main {
+    public static void main(String[] args) {
+        Camera camera = new Camera(10);
+        Handy handy = new Handy("Huragweih","red0",camera);
+        handy.turnOn();
+        handy.getCamera().takePicture();
+    }
+}

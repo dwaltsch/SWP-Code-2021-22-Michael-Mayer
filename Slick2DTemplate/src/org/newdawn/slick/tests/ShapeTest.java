e467a20c11829d57e7327ae3c09aa674fb358cf3
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-02-02 08:10:35 +0100

    gooooooooooooooooooooooooooooooooood morning

--- Slick2DTemplate/src/org/newdawn/slick/tests/ShapeTest.java
+++ Slick2DTemplate/src/org/newdawn/slick/tests/ShapeTest.java
@@ -23,48 +23,82 @@ import org.newdawn.slick.opengl.renderer.Renderer;
  * @author kevin
  */
 public class ShapeTest extends BasicGame {
-    /** rectangle to display */
+    /**
+     * rectangle to display
+     */
     private Rectangle rect;
-    /** rounded rectangle to display */
+    /**
+     * rounded rectangle to display
+     */
     private RoundedRectangle roundRect;
-    /** ellipse to display */
+    /**
+     * ellipse to display
+     */
     private Ellipse ellipse;
-    /** circle to display */
+    /**
+     * circle to display
+     */
     private Circle circle;
-    /** polygon to display */
+    /**
+     * polygon to display
+     */
     private Polygon polygon;
-    /** list for drawing the shapes*/
+    /**
+     * list for drawing the shapes
+     */
     private ArrayList shapes;
-    /** track key presses */
+    /**
+     * track key presses
+     */
     private boolean keys[];
-    /** since no modifiers, use this for shifted characters */
+    /**
+     * since no modifiers, use this for shifted characters
+     */
     private char lastChar[];
-    /** The polgon randomly generated */
+    /**
+     * The polgon randomly generated
+     */
     private Polygon randomShape = new Polygon();
-    
+
     /**
      * Create a new test of graphics context rendering
      */
     public ShapeTest() {
         super("Geom Test");
     }
-	
-	public void createPoly(float x, float y) {
-		int size = 20;
-		int change = 10;
 
-		randomShape = new Polygon();
-		// generate random polygon
-		randomShape.addPoint(0 + (int)(Math.random() * change), 0 + (int)(Math.random() * change));
-		randomShape.addPoint(size - (int)(Math.random() * change), 0 + (int)(Math.random() * change));
-		randomShape.addPoint(size - (int)(Math.random() * change), size - (int)(Math.random() * change));
-		randomShape.addPoint(0 + (int)(Math.random() * change), size - (int)(Math.random() * change));
-	
-		// center polygon
-		randomShape.setCenterX(x);
-		randomShape.setCenterY(y);
-	}
-	
+    /**
+     * Entry point to our test
+     *
+     * @param argv The arguments passed to the test
+     */
+    public static void main(String[] argv) {
+        try {
+            Renderer.setRenderer(Renderer.VERTEX_ARRAY_RENDERER);
+            AppGameContainer container = new AppGameContainer(new ShapeTest());
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createPoly(float x, float y) {
+        int size = 20;
+        int change = 10;
+
+        randomShape = new Polygon();
+        // generate random polygon
+        randomShape.addPoint(0 + (int) (Math.random() * change), 0 + (int) (Math.random() * change));
+        randomShape.addPoint(size - (int) (Math.random() * change), 0 + (int) (Math.random() * change));
+        randomShape.addPoint(size - (int) (Math.random() * change), size - (int) (Math.random() * change));
+        randomShape.addPoint(0 + (int) (Math.random() * change), size - (int) (Math.random() * change));
+
+        // center polygon
+        randomShape.setCenterX(x);
+        randomShape.setCenterY(y);
+    }
+
     /**
      * @see org.newdawn.slick.BasicGame#init(org.newdawn.slick.GameContainer)
      */
@@ -80,10 +114,10 @@ public class ShapeTest extends BasicGame {
         shapes.add(circle);
         polygon = new Polygon(new float[]{550, 10, 600, 40, 620, 100, 570, 130});
         shapes.add(polygon);
-        
+
         keys = new boolean[256];
         lastChar = new char[256];
-        createPoly(200,200);
+        createPoly(200, 200);
     }
 
     /**
@@ -91,16 +125,16 @@ public class ShapeTest extends BasicGame {
      */
     public void render(GameContainer container, Graphics g) {
         g.setColor(Color.green);
-        
-        for(int i=0;i<shapes.size();i++) {
-            g.fill((Shape)shapes.get(i));
+
+        for (int i = 0; i < shapes.size(); i++) {
+            g.fill((Shape) shapes.get(i));
         }
         g.fill(randomShape);
         g.setColor(Color.black);
         g.setAntiAlias(true);
         g.draw(randomShape);
         g.setAntiAlias(false);
-        
+
         g.setColor(Color.white);
         g.drawString("keys", 10, 300);
         g.drawString("wasd - move rectangle", 10, 315);
@@ -119,141 +153,129 @@ public class ShapeTest extends BasicGame {
      * @see org.newdawn.slick.BasicGame#update(org.newdawn.slick.GameContainer, int)
      */
     public void update(GameContainer container, int delta) {
-        
-        createPoly(200,200);
-        if(keys[Input.KEY_ESCAPE]) {
+
+        createPoly(200, 200);
+        if (keys[Input.KEY_ESCAPE]) {
             System.exit(0);
         }
-        if(keys[Input.KEY_W]) {
-            if(lastChar[Input.KEY_W] == 'w') {
+        if (keys[Input.KEY_W]) {
+            if (lastChar[Input.KEY_W] == 'w') {
                 rect.setY(rect.getY() - 1);
-            }
-            else {
+            } else {
                 rect.setHeight(rect.getHeight() - 1);
             }
         }
-        if(keys[Input.KEY_S]) {
-            if(lastChar[Input.KEY_S] == 's') {
+        if (keys[Input.KEY_S]) {
+            if (lastChar[Input.KEY_S] == 's') {
                 rect.setY(rect.getY() + 1);
-            }
-            else {
+            } else {
                 rect.setHeight(rect.getHeight() + 1);
             }
         }
-        if(keys[Input.KEY_A]) {
-            if(lastChar[Input.KEY_A] == 'a') {
+        if (keys[Input.KEY_A]) {
+            if (lastChar[Input.KEY_A] == 'a') {
                 rect.setX(rect.getX() - 1);
-            }
-            else {
+            } else {
                 rect.setWidth(rect.getWidth() - 1);
             }
         }
-        if(keys[Input.KEY_D]) {
-            if(lastChar[Input.KEY_D] == 'd') {
+        if (keys[Input.KEY_D]) {
+            if (lastChar[Input.KEY_D] == 'd') {
                 rect.setX(rect.getX() + 1);
-            }
-            else {
+            } else {
                 rect.setWidth(rect.getWidth() + 1);
             }
         }
-        if(keys[Input.KEY_T]) {
-            if(lastChar[Input.KEY_T] == 't') {
+        if (keys[Input.KEY_T]) {
+            if (lastChar[Input.KEY_T] == 't') {
                 roundRect.setY(roundRect.getY() - 1);
-            }
-            else {
+            } else {
                 roundRect.setHeight(roundRect.getHeight() - 1);
             }
         }
-        if(keys[Input.KEY_G]) {
-            if(lastChar[Input.KEY_G] == 'g') {
+        if (keys[Input.KEY_G]) {
+            if (lastChar[Input.KEY_G] == 'g') {
                 roundRect.setY(roundRect.getY() + 1);
-            }
-            else {
+            } else {
                 roundRect.setHeight(roundRect.getHeight() + 1);
             }
         }
-        if(keys[Input.KEY_F]) {
-            if(lastChar[Input.KEY_F] == 'f') {
+        if (keys[Input.KEY_F]) {
+            if (lastChar[Input.KEY_F] == 'f') {
                 roundRect.setX(roundRect.getX() - 1);
-            }
-            else {
+            } else {
                 roundRect.setWidth(roundRect.getWidth() - 1);
             }
         }
-        if(keys[Input.KEY_H]) {
-            if(lastChar[Input.KEY_H] == 'h') {
+        if (keys[Input.KEY_H]) {
+            if (lastChar[Input.KEY_H] == 'h') {
                 roundRect.setX(roundRect.getX() + 1);
-            }
-            else {
+            } else {
                 roundRect.setWidth(roundRect.getWidth() + 1);
             }
         }
-        if(keys[Input.KEY_R]) {
+        if (keys[Input.KEY_R]) {
             roundRect.setCornerRadius(roundRect.getCornerRadius() - 1);
         }
-        if(keys[Input.KEY_Y]) {
+        if (keys[Input.KEY_Y]) {
             roundRect.setCornerRadius(roundRect.getCornerRadius() + 1);
         }
-        if(keys[Input.KEY_I]) {
-            if(lastChar[Input.KEY_I] == 'i') {
+        if (keys[Input.KEY_I]) {
+            if (lastChar[Input.KEY_I] == 'i') {
                 ellipse.setCenterY(ellipse.getCenterY() - 1);
-            }
-            else {
+            } else {
                 ellipse.setRadius2(ellipse.getRadius2() - 1);
             }
         }
-        if(keys[Input.KEY_K]) {
-            if(lastChar[Input.KEY_K] == 'k') {
+        if (keys[Input.KEY_K]) {
+            if (lastChar[Input.KEY_K] == 'k') {
                 ellipse.setCenterY(ellipse.getCenterY() + 1);
-            }
-            else {
+            } else {
                 ellipse.setRadius2(ellipse.getRadius2() + 1);
             }
         }
-        if(keys[Input.KEY_J]) {
-            if(lastChar[Input.KEY_J] == 'j') {
+        if (keys[Input.KEY_J]) {
+            if (lastChar[Input.KEY_J] == 'j') {
                 ellipse.setCenterX(ellipse.getCenterX() - 1);
-            }
-            else {
+            } else {
                 ellipse.setRadius1(ellipse.getRadius1() - 1);
             }
         }
-        if(keys[Input.KEY_L]) {
-            if(lastChar[Input.KEY_L] == 'l') {
+        if (keys[Input.KEY_L]) {
+            if (lastChar[Input.KEY_L] == 'l') {
                 ellipse.setCenterX(ellipse.getCenterX() + 1);
-            }
-            else {
+            } else {
                 ellipse.setRadius1(ellipse.getRadius1() + 1);
             }
         }
-        if(keys[Input.KEY_UP]) {
+        if (keys[Input.KEY_UP]) {
             circle.setCenterY(circle.getCenterY() - 1);
         }
-        if(keys[Input.KEY_DOWN]) {
+        if (keys[Input.KEY_DOWN]) {
             circle.setCenterY(circle.getCenterY() + 1);
         }
-        if(keys[Input.KEY_LEFT]) {
+        if (keys[Input.KEY_LEFT]) {
             circle.setCenterX(circle.getCenterX() - 1);
         }
-        if(keys[Input.KEY_RIGHT]) {
+        if (keys[Input.KEY_RIGHT]) {
             circle.setCenterX(circle.getCenterX() + 1);
         }
-        if(keys[Input.KEY_PRIOR]) {
+        if (keys[Input.KEY_PRIOR]) {
             circle.setRadius(circle.getRadius() - 1);
         }
-        if(keys[Input.KEY_NEXT]) {
+        if (keys[Input.KEY_NEXT]) {
             circle.setRadius(circle.getRadius() + 1);
         }
-        if(keys[Input.KEY_NUMPAD8]) {
+        if (keys[Input.KEY_NUMPAD8]) {
             polygon.setY(polygon.getY() - 1);
         }
-        if(keys[Input.KEY_NUMPAD5]) {
+        if (keys[Input.KEY_NUMPAD5]) {
             polygon.setY(polygon.getY() + 1);
         }
-        if(keys[Input.KEY_NUMPAD4]) {
+        if (keys[Input.KEY_NUMPAD4]) {
             polygon.setX(polygon.getX() - 1);
         }
-        if(keys[Input.KEY_NUMPAD6]) {
+        if (keys[Input.KEY_NUMPAD6]) {
             polygon.setX(polygon.getX() + 1);
         }
     }
@@ -265,27 +287,11 @@ public class ShapeTest extends BasicGame {
         keys[key] = true;
         lastChar[key] = c;
     }
-    
+
     /**
      * @see org.newdawn.slick.BasicGame#keyReleased(int, char)
      */
     public void keyReleased(int key, char c) {
         keys[key] = false;
-    }
-
-    /**
-     * Entry point to our test
-     * 
-     * @param argv The arguments passed to the test
-     */
-    public static void main(String[] argv) {
-        try {
-        	Renderer.setRenderer(Renderer.VERTEX_ARRAY_RENDERER);
-            AppGameContainer container = new AppGameContainer(new ShapeTest());
-            container.setDisplayMode(800,600,false);
-            container.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
     }
 }
--- Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/rectangle/Rectangle.java
+++ Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/rectangle/Rectangle.java
@@ -6,8 +6,19 @@ import org.newdawn.slick.tests.AnimationTest;
 public class Rectangle extends BasicGame {
     private int x;
     private int y;
+
     public Rectangle(String title) {
         super(title);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangle("Test"));
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -18,29 +29,19 @@ public class Rectangle extends BasicGame {
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
-            this.x++;
-            if(this.x > 800){
-                this.x = 0;
-            }
+        this.x++;
+        if (this.x > 800) {
+            this.x = 0;
+        }
         this.y++;
-        if(this.y > 800){
+        if (this.y > 800) {
             this.y = 0;
         }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(x,y,100,100);
-        graphics.drawString("Grüzi",69,69);
-    }
-
-    public static void main(String[] argv) {
-        try {
-            AppGameContainer container = new AppGameContainer(new Rectangle("Test"));
-            container.setDisplayMode(800,600,false);
-            container.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
+        graphics.drawRect(x, y, 100, 100);
+        graphics.drawString("Grüzi", 69, 69);
     }
 }

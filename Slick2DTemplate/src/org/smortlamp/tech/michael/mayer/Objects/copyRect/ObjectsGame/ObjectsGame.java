da4548c937a5dd8d1eeea4a5cc277eff7792ce06
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-02-09 08:49:24 +0100

    Rectangle finished sichael vom 2 nicht dichael vom

--- Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/Objects/copyRect/ObjectsGame/ObjectsGame.java
+++ Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/Objects/copyRect/ObjectsGame/ObjectsGame.java
@@ -3,18 +3,9 @@ package org.smortlamp.tech.michael.mayer.Objects.copyRect.ObjectsGame;
 import org.newdawn.slick.*;
 
 public class ObjectsGame extends BasicGame {
-    private float x;
 
-    ;
-    private float y;
-    private DIRECTION objectDirection;
-    private float x1;
-    private float y1;
-    private DIRECTION object1Direction;
-    private float x2;
-    private float y2;
-    private DIRECTION object2Direction;
-    private float speed;
+
+    private Rectangle rectangle;
 
     public ObjectsGame(String title) {
         super(title);
@@ -32,71 +23,16 @@ public class ObjectsGame extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.x = 100;
-        this.speed = 2.0f;
-        this.objectDirection = DIRECTION.RIGHT;
-
-        this.y1 = 50;
-        this.object1Direction = DIRECTION.DOWN;
-
-        this.x2 = 50;
-        this.object2Direction = DIRECTION.RIGHT;
+        this.rectangle = new Rectangle(0,0,1);
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        if (this.object1Direction == DIRECTION.DOWN) {
-            this.y1 += delta / this.speed;
-            if (this.y1 > 500) {
-                this.object1Direction = DIRECTION.UP;
-            }
-        } else {
-            this.y1 -= delta / this.speed;
-            if (this.y1 < 100) {
-                this.object1Direction = DIRECTION.DOWN;
-            }
-        }
-        if (this.objectDirection == DIRECTION.RIGHT) {
-            this.x += delta / this.speed;
-            if (this.x > 500) {
-                this.objectDirection = DIRECTION.LEFT;
-            }
-        } else {
-            this.x -= delta / this.speed;
-            if (this.x < 100) {
-                this.objectDirection = DIRECTION.RIGHT;
-            }
-        }
-        if (this.object2Direction == DIRECTION.RIGHT) {
-            this.x2 += delta / this.speed;
-            if (this.x2 > 500) {
-                this.object2Direction = DIRECTION.DOWN;
-            }
-        }
-        if (this.object2Direction == DIRECTION.DOWN) {
-            this.y2 += delta / this.speed;
-            if (this.y2 > 500) {
-                this.object2Direction = DIRECTION.LEFT;
-            }
-        } else if (this.object2Direction == DIRECTION.LEFT) {
-            this.x2 -= delta / this.speed;
-            if (this.x2 < 100) {
-                this.object2Direction = DIRECTION.UP;
-            }
-        } else if (this.object2Direction == DIRECTION.UP) {
-            this.y2 -= delta / this.speed;
-            if (this.y2 < 100) {
-                this.object2Direction = DIRECTION.RIGHT;
-            }
-        }
-    }
+        this.rectangle.update(delta);
 
+    }
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawOval(x1, y1, 50, 50);
-        graphics.drawOval(x, y, 50, 50);
-        graphics.drawRect(x2, y2, 50, 50);
+        this.rectangle.render(graphics);
     }
-
-    private enum DIRECTION {RIGHT, DOWN, LEFT, UP}
 }
--- Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/Objects/copyRect/ObjectsGame/readme.md
+++ Slick2DTemplate/src/org/smortlamp/tech/michael/mayer/Objects/copyRect/ObjectsGame/readme.md
@@ -3,3 +3,5 @@ https://www.youtube.com/watch?v=dolTEfF-mYA
 https://www.youtube.com/watch?v=uoJDfaY_o5I
 
 https://www.youtube.com/watch?v=bwQ-7MdzGJQ
+
+![](.readme_images/1586df44.png)

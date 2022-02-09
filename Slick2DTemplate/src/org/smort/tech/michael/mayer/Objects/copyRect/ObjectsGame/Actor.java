56d9449e8eedc363f8f589ee5e783cbe1cde798c
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-02-09 09:31:42 +0100

    Interface https://www.youtube.com/watch?v=0o9MeiONTno 3:10

---
+++ Slick2DTemplate/src/org/smort/tech/michael/mayer/Objects/copyRect/ObjectsGame/Actor.java
@@ -0,0 +1,6 @@
+package org.smort.tech.michael.mayer.Objects.copyRect.ObjectsGame;
+
+public interface Actor {
+   public void render(Graphics graphics);
+   public void update(int delta);
+}
---
+++ Slick2DTemplate/src/org/smort/tech/michael/mayer/Objects/copyRect/ObjectsGame/Elipse.java
@@ -0,0 +1,13 @@
+package org.smort.tech.michael.mayer.Objects.copyRect.ObjectsGame;
+
+public class Elipse implements Actor{
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+
+    @Override
+    public void update(int delta) {
+
+    }
+}
